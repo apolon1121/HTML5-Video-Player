057fe0e5c93058906b83a9ca3a85c7feece4090e
Apolon <a.pachulia@hotmail.com>
2016-12-31 22:10:52 +0400

    timing

--- player.js
+++ player.js
@@ -4,6 +4,8 @@ window.addEventListener('load',function() {
   playButton = document.getElementById('play-button');
   progressbarContainer = document.getElementById('progressbar-container');
   progressbar = document.getElementById('progressbar');
+  timeField = document.getElementById('time-field');
+
   video.load();
   video.addEventListener('canplay',function() {
 
@@ -29,7 +31,7 @@ function playOrPause() {
 function updatePlayer() {
   var percentage = (video.currentTime/video.duration)*100;
   progressbar.style.width=percentage+'%';
-
+  timeField.innerHTML = getFormattedTime();
   if(video.ended) {
     window.clearInterval(update);
     playButton.src = 'images/replay.png';
@@ -42,4 +44,12 @@ function skip(event) {
   var width = window.getComputedStyle(progressbarContainer).getPropertyValue('width');
   width = parseFloat(width.substr(0, width.length-2));
   video.currentTime = (mouseX/width)*video.duration;
+  updatePlayer();
+}
+
+function getFormattedTime() {
+  var seconds = Math.round(video.currentTime);
+  var minutes = Math.floor(seconds/60);
+  if(seconds.toString().length===1) seconds = '0'+seconds;
+  return minutes+':'+seconds;
 }
